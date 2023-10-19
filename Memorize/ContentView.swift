dfa472d0781454f8882e0dc9dee0d4fbade42063
Dawae111 <DavidApply2022@outlook.com>
2023-10-18 20:17:37 -0400

    Creation of Card View IsFaceup

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,22 +7,34 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View { //behaves like a view\
     var body: some View {
-        ZStack{
-            Color.red.ignoresSafeArea(.all)
-            
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("This is David")
-            }
-            .padding()
+        HStack{
+            CardView(isFaceup: true)
+            CardView()
         }
+        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+        .padding()
     }
 }
 
+
+struct CardView: View{
+    var isFaceup:Bool = false;
+    var body: some View{
+        ZStack {
+            if isFaceup{
+                RoundedRectangle(cornerRadius: 25)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                    .strokeBorder(style: StrokeStyle(lineWidth: 10, dash:[10,2]))
+                Text("😩").font(.largeTitle)
+            }else{
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+            }
+        }
+    }
+}
 #Preview {
     ContentView()
 }
